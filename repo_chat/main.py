6079ac86eed14544f0d9911b6e0e2f2eeeef946e
Yeok-c <71489785+Yeok-c@users.noreply.github.com>
2023-09-03 19:08:44 +0800

    Update main.py

--- repo_chat/main.py
+++ repo_chat/main.py
@@ -11,59 +11,135 @@ from langchain.chains import ConversationalRetrievalChain
 # import tiktoken
 from langchain.callbacks import get_openai_callback
 import os, shutil
-
+from timeit import default_timer as timer
+import time
+import numpy as np
 
 class RepoChat():
-    def __init__(self, codebase_path: str, model_name="gpt-3.5-turbo-16k", suffixes=[".py", ".ipynb", ".md"]):                
+    def __init__(self, 
+                 codebase_path: str, 
+                 codebase_local_path: str = "./temp_repo/",
+                 db_persist_directory: str = "chroma_db",
+                 query_tokens: int = 4000,
+                 db_creation_mode: str = "local",
+                 model_name="gpt-3.5-turbo", 
+                 suffixes=[".py", ".ipynb", ".md"]):                
         self.model_name = model_name
         self.cb = None
+        self.codebase_local_path = codebase_local_path
+        self.db_persist_directory = db_persist_directory
+        self.query_tokens = query_tokens
+        self.suffixes = suffixes
+        
+        if db_creation_mode == "local":
+            db=self.db_from_local(codebase_path)
+        elif db_creation_mode == "github":
+            if "github.com" in codebase_path: # if it is a github
+                db=self.db_from_github(codebase_path)
+        elif db_creation_mode == "persist":
+            db=self.db_from_persist()
 
-        if "github.com" in codebase_path: # if it is a github
-            codebase_local_path = "./temp_repo/"
-            if os.path.isdir(codebase_local_path): shutil.rmtree(codebase_local_path, )
-            print(f"Cloning from {codebase_path} to {codebase_local_path}")
-            repo = Repo.clone_from(codebase_path, to_path=codebase_local_path)
-            codebase_path = codebase_local_path
-        else: #if it is local
-            print("Loading from local repo codebase_path")
+        retriever = db.as_retriever(
+            search_type="mmr",  # Also test "similarity"
+            search_kwargs={"k": 8},
+        )
+
+        with get_openai_callback() as cb:
+            # Set up LLM with memory
+            llm = ChatOpenAI(model_name=self.model_name) 
+            memory = ConversationSummaryMemory(llm=llm,memory_key="chat_history",return_messages=True)
+            self.qa = ConversationalRetrievalChain.from_llm(
+                llm, 
+                retriever=retriever, 
+                memory=memory,
+                max_tokens_limit=self.query_tokens,
+            )
+        self.update_usage(cb)
+
+        print("Repo loaded into langchain.")
+        result = self.chat("Based on the readme, main program, or any other overview, describe this repo.")
+        print(result)
+        self.print_usage()
+
+    def db_from_github(self, codebase_path):
+        if os.path.isdir(self.codebase_local_path): shutil.rmtree(self.codebase_local_path)
+        print(f"Cloning from {codebase_path} to {self.codebase_local_path}")
+        repo = Repo.clone_from(codebase_path, to_path=self.codebase_local_path)
+        
+        self.from_local(self.codebase_local_path)
+
+    def db_from_local(self, codebase_path):
+        print(f"Loading from {codebase_path}")
         
         # Load
         loader = GenericLoader.from_filesystem(
             codebase_path,
             glob="**/*",
-            suffixes=suffixes,
+            suffixes=self.suffixes,
             parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
             show_progress=True,
         )
         documents = loader.load()
         # len(documents)
 
-        python_splitter = RecursiveCharacterTextSplitter.from_language(language=Language.PYTHON, 
-                                                                    chunk_size=2000, 
-                                                                    chunk_overlap=200,)
+        python_splitter = RecursiveCharacterTextSplitter.from_language(
+            language=Language.PYTHON, 
+            chunk_size=3600, 
+            chunk_overlap=200,
+            )
         texts = python_splitter.split_documents(documents)
 
-        print(f"Loaded {len(documents)} documents, split into {len(texts)} texts. \nLoading into chroma. This may take a while.")
+        chunk_size = 300
+        n_chunks = np.ceil(len(texts)/chunk_size)
 
-        with get_openai_callback() as cb:
-            # Set up chroma db and retriever
-            db = Chroma.from_documents(texts, OpenAIEmbeddings(disallowed_special=()))
-            retriever = db.as_retriever(
-                search_type="mmr",  # Also test "similarity"
-                search_kwargs={"k": 8},
+        print(f"Loaded {len(documents)} documents, \
+              split into {len(texts)} texts, \
+                grouped into {n_chunks} of size {chunk_size} chunks. \
+                    \nLoading into chroma. This may take a while.")
+
+        def _split_list(lst, chunk_size):
+            for i in range(0, len(lst), chunk_size):
+            # for i in range(len(lst)-2, len(lst), chunk_size):
+                yield lst[i:i+chunk_size]
+
+        # Set up chroma db and retriever
+
+        # db = Chroma.from_documents(
+        #     texts, 
+        #     OpenAIEmbeddings(disallowed_special=(),),
+        #     persist_directory=persist_directory
+        #     )
+
+        db = Chroma(
+            persist_directory=self.persist_directory, 
+            embedding_function=OpenAIEmbeddings(
+                disallowed_special=(),
+                )
             )
-
-            # Set up LLM with memory
-            llm = ChatOpenAI(model_name=self.model_name) 
-            memory = ConversationSummaryMemory(llm=llm,memory_key="chat_history",return_messages=True)
-            self.qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
         
-        print("Repo loaded into langchain.")
-        # self.update_usage(cb)
-        # self.print_usage()
-        result = self.qa("Give a summary of the repo you have access to based on its readme, main program, or any other useful code you can find.")
-        print(result['answer'])
+        # Max is 1,000,000 per minute
+        for i, chunk in enumerate(_split_list(texts, chunk_size)):
+            print(f"Loading documents into chroma_db {i}/{n_chunks}")
+            start = timer()
+            db.add_documents(documents=chunk)
+            # while (timer() - start) < 60: # Time in seconds, e.g. 5.38091952400282
+            if (timer() - start) < 60:
+                print(f"Waiting, {60 - (timer() - start)}")
+                time.sleep(60 + 1 - (timer() - start))
+            else:
+                # pass
+                print(f"Done in {timer() - start}, over one minute, not waiting.")
+                # print(chunk)
 
+        return db
+
+    def db_from_persist(self):
+        db = Chroma(
+                persist_directory=self.db_persist_directory, 
+                embedding_function=OpenAIEmbeddings(
+                        disallowed_special=(),)
+            )
+        return db
 
     def update_usage(self, cb):
         if self.cb is None:
